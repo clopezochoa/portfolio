ca4c8a444046436b8a06076aa3ecd9c98b17d5ef
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-27 19:23:54 +0100

    fonts

---
+++ src/app/providers/fonts.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { NextFont } from "next/dist/compiled/@next/font";
+import { Fraunces, Mulish, Encode_Sans_Condensed } from "next/font/google";
+
+export const serif = Fraunces({ weight: ["200", "300", "400", "500", "600", "700"], subsets: ["latin"] });
+export const sans = Mulish({ weight: ["400", "700"], subsets: ["latin"] });
+export const condensed = Encode_Sans_Condensed({ weight: ["400", "700"], subsets: ["latin"] });
+
+import React, { ReactNode, createContext } from 'react'
+
+interface FontsModel {
+  serif: NextFont;
+  sans: NextFont;
+  condensed: NextFont;
+}
+
+const defaultFonts = {serif: serif, sans: sans, condensed: condensed};
+
+export const FontsContext = createContext<FontsModel>(defaultFonts);
+function Fonts({children}: {children: ReactNode}) {
+  return (
+    <>
+      <FontsContext.Provider value={defaultFonts}>
+        {children}
+      </FontsContext.Provider>
+    </>
+  )
+}
+
+export default Fonts
