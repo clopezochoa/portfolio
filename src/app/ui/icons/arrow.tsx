e237df66cd8be24bd6bc9baae1d2e939c418a66f
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-27 19:25:14 +0100

    arrow component

---
+++ src/app/ui/icons/arrow.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { CSSProperties } from "react";
+
+function Arrow({className, width, height, stroke, onClick}:{className:string, width:number, height:number, stroke:string, onClick?:() => void}) {
+  const style: CSSProperties   = {
+    fill: "none",
+    stroke: "#E8E8E8",
+    strokeLinecap: "round",
+    strokeMiterlimit: "10",
+    strokeWidth: stroke,
+    width: width.toString() + "px",
+    height: height.toString() + "px"
+  }
+  return (
+    <div onClick={onClick} className={className}>
+      <svg style={style} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98.3 79.72">
+        <path d="M49.15.5c0,26.87-21.78,48.65-48.65,48.65"/>
+        <path d="M49.15.5c0,26.87,21.78,48.65,48.65,48.65"/>
+        <line x1="49.15" y1=".5" x2="49.15" y2="79.22"/>
+      </svg>
+    </div>
+
+  )
+}
+
+export default Arrow
