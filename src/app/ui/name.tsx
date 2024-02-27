930f640937670dcaca3b81dd26605c2256110c79
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-27 19:26:41 +0100

    name header component

---
+++ src/app/ui/name.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '@/styles/text.css'
+import { NextFont } from 'next/dist/compiled/@next/font'
+
+interface NameProps {
+  name: string;
+  surname: string;
+  font: NextFont;
+}
+
+function Name(nameProps: NameProps) {
+  return (<>
+  <div className='name-container'>
+    <div className={`name ${nameProps.font.className}`}>
+      <h1>{nameProps.name}</h1>
+      <h2>{nameProps.surname}</h2>
+    </div>
+  </div>
+  </>
+  )
+}
+
+export default Name
