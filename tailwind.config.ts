42e30dc86ff156e86aa566f822af94e5026c56ae
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-27 19:23:30 +0100

    colors config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,31 @@
 import type { Config } from "tailwindcss";
 
+export interface ColorsModel {
+  clear: string;
+  medium: string;
+  dark: string;
+  cream: string;
+  lightGreen: string;
+  green: string;
+  darkGreen: string;
+  purple: string;
+  yellow: string;
+  red: string;
+}
+
+export const defaultColors = {
+  clear: "#E8E8E8",
+  medium: "#888888",
+  dark: "#3D3D3D",
+  cream: "#F8FFE5",
+  lightGreen: "#B2E6D4",
+  green: "#898952",
+  darkGreen: "#082D0F",
+  purple: "#694873",
+  yellow: "#FAA916",
+  red: "#CD5D67"
+};
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +39,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "clear": defaultColors.clear,
+        "medium": defaultColors.medium,
+        "dark": defaultColors.dark,
+        "cream": defaultColors.cream,
+        "light-green": defaultColors.lightGreen,
+        "green": defaultColors.green,
+        "dark-green": defaultColors.darkGreen,
+        "purple": defaultColors.purple,
+        "yellow": defaultColors.yellow,
+        "red": defaultColors.red,
+      }
     },
   },
   plugins: [],
