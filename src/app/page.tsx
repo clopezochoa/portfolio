cb8b1ac5d6b2768da2ede08cfac608e4612c63d9
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-02-28 17:40:39 +0100

    new router on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,23 @@ import Landing from "./ui/landing";
 import Arrow from '@/app/ui/icons/arrow';
 import { useRouter } from "next/navigation";
 import useScrollEvent from "./hooks/useScrollEvent";
+import { Route, getRouteGroup } from "@/lib/routeList";
 
 export default function Home() {
   const fonts = useContext(FontsContext);
   const router = useRouter();
+  const routeGroup = getRouteGroup(Route.home);
 
   useScrollEvent(() => {
-    router.push('/about-me')
-  }, undefined, true);
+    router.push(routeGroup.up)
+  }, () => {
+    router.push(routeGroup.down)
+  }, true);
 
   return (<>
   <div>
-    <Arrow width={"auto"} height={60} stroke="1pt" className='arrow absolute top left' onClick={() => router.push('/about-me')}/>
-    <Arrow width={"auto"} height={60} stroke="1pt" className='arrow  absolute bottom left mirror' />
+    <Arrow width={"auto"} height={60} stroke="1pt" className='arrow absolute top left' onClick={() => router.push(routeGroup.up)}/>
+    <Arrow width={"auto"} height={60} stroke="1pt" className='arrow  absolute bottom left mirror' onClick={() => router.push(routeGroup.down)}/>
     <Name name="Carlos" surname="López-Ochoa" font={fonts.serif} />
     <Landing font={fonts.serif} />
   </div>
