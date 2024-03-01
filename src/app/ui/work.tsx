91a09245fb81307507bd02cda76dc7452ce0d20b
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-01 10:32:29 +0100

    work component

---
+++ src/app/ui/work.tsx
@@ -0,0 +1,47 @@
+import { NextFont } from 'next/dist/compiled/@next/font';
+import '@/styles/text.css'
+import React, { CSSProperties } from 'react'
+
+interface WorkTitleProps {
+  title: string | React.JSX.Element;
+  font: NextFont;
+  style: CSSProperties;
+}
+
+export function WorkTitle(workTitleProps: WorkTitleProps) {
+  return (<>
+    <div style={workTitleProps.style} className={`text-clear story-title-main ${workTitleProps.font.className}`}>
+      <h1>{workTitleProps.title}</h1>
+    </div>
+  </>)
+}
+
+interface WorkHeaderProps {
+  title: string;
+  subtitle: string | React.JSX.Element;
+  font: NextFont;
+}
+
+export function WorkHeader(workHeaderProps: WorkHeaderProps) {
+  return (<>
+    <div className={`text-clear work ${workHeaderProps.font.className}`}>
+      <h1>{workHeaderProps.title}</h1>
+      <h2 className='mt-2'>{workHeaderProps.subtitle}</h2>
+    </div>
+  </>)
+}
+
+interface WorkBodyProps {
+  body: Array<string | React.JSX.Element>;
+  font: NextFont;
+}
+
+export function WorkBody(workBodyProps: WorkBodyProps) {
+  return (<>
+  <div className={'work-body-container story-body-main text-clear ' + workBodyProps.font.className} >
+    {workBodyProps.body.map((item, key) => 
+        <div key={key} style={{marginTop:"20pt"}}>{item}</div>
+    )}
+  </div>
+  </>)
+}
