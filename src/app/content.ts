456d483829539ed3fda3427dea73978b4116f9f3
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-01 12:57:53 +0100

    keywords

---
+++ src/app/content.ts
@@ -0,0 +1,14 @@
+export const keywords = [
+  "3D",
+  "Frontend",
+  "Front",
+  "Front-end",
+  "Design",
+  "Developer",
+  "Designer",
+  "Candidate",
+  "Carlos",
+  "Lopez",
+  "Ochoa",
+  "Aledo"
+];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,13 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { defaultColors } from "../../tailwind.config";
 import Providers from "./providers";
+import { keywords } from "./content";
 
 export const metadata: Metadata = {
   title: "Carlos López-Ochoa",
   description: "Frontend Developer",
+  keywords: keywords
+  
 };
 
 export default function RootLayout({
@@ -16,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en" style={{backgroundColor:defaultColors.dark}}>
       <Providers>
-        <body>{children}</body>
+        <body>
+          {children}
+        </body>
       </Providers>
     </html>
   );
