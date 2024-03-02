09d842bea18082c6f6ce22287bbf0b1a4ea7e942
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-02 22:28:24 +0100

    landing page refactor arrows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,44 @@
 'use client'
 
 import Name from "./ui/name";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { FontsContext } from "./providers/fonts";
 import Landing from "./ui/landing";
 import Arrow from '@/app/ui/icons/arrow';
 import '@/styles/utils.css'
 import '@/styles/text.css'
 import GenerativeAlgorithm from "./generative-algorithm/content";
+import Window from "@/app/ui/window";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const fonts = useContext(FontsContext);
+  const router = useRouter();
+  const mainDiv = useRef(null);
+
+  const showAboutMe = () => {
+    (mainDiv.current! as HTMLDivElement).className = "slide-down";
+    
+    setTimeout(() => {
+      router.push("/about");
+    }, 1000);
+  }
 
   return (<>
-    <div className="grid">
-      <div className="landing-container">
-        <Arrow width={"auto"} height={60} stroke="1pt" orientation='up' className='arrow absolute top left' onClick={() => null}/>
-        <Arrow width={"auto"} height={60} stroke="1pt" orientation='down' className='arrow absolute bottom left' /* onClick={() => router.push(routeGroup.down)} *//>
-        <Name name="Carlos" surname="López-Ochoa" font={fonts.serif} />
-        <Landing font={fonts.serif} />
-      </div>
-      <div className="content-below">
+    <div ref={mainDiv} className="grid">
+      <Window className="flex">
+        <div className="arrow-deck">
+          <Arrow width={"auto"} height={60} stroke="1pt" orientation='up' className='arrow' onClick={() => showAboutMe()}/>
+          <Arrow width={"auto"} height={60} stroke="1pt" orientation='down' className='arrow' /* onClick={() => router.push(routeGroup.down)} *//>
+        </div>
+        <div className="landing-container">
+          <Name name="Carlos" surname="López-Ochoa" font={fonts.serif} />
+          <Landing font={fonts.serif} />
+        </div>
+      </Window>
+      <Window className="space-between">
         <GenerativeAlgorithm />
-      </div>
+      </Window>
     </div>
   </>
   );
