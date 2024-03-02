5ee23d7f5ef7003ba51f8a105d5f2aca39b8a38f
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-02 18:53:30 +0100

    move landing page contents into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 'use client'
 
-import HomePage from '@/app'
+import Name from "./ui/name";
+import { useContext } from "react";
+import { FontsContext } from "./providers/fonts";
+import Landing from "./ui/landing";
+import Arrow from '@/app/ui/icons/arrow';
+import '@/styles/utils.css'
+import '@/styles/text.css'
+import GenerativeAlgorithm from "./generative-algorithm/content";
 
 export default function Home() {
+  const fonts = useContext(FontsContext);
 
   return (<>
-    <div style={{overflow:"hidden"}}>
-      <HomePage />
+    <div className="grid">
+      <div className="landing-container">
+        <Arrow width={"auto"} height={60} stroke="1pt" orientation='up' className='arrow absolute top left' onClick={() => null}/>
+        <Arrow width={"auto"} height={60} stroke="1pt" orientation='down' className='arrow absolute bottom left' /* onClick={() => router.push(routeGroup.down)} *//>
+        <Name name="Carlos" surname="López-Ochoa" font={fonts.serif} />
+        <Landing font={fonts.serif} />
+      </div>
+      <div className="content-below">
+        <GenerativeAlgorithm />
+      </div>
     </div>
   </>
   );
