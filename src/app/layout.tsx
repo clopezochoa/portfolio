8be266ac9a865a235f4174f6239ad3a3ae6246cb
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-02 18:52:02 +0100

    set default layout body bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { defaultColors } from "../../tailwind.config";
 import Providers from "./providers";
 import { keywords } from "./content";
 
@@ -17,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" style={{backgroundColor:defaultColors.dark}}>
+    <html lang="en">
       <Providers>
-        <body>
+        <body className="dark-bg">
           {children}
         </body>
       </Providers>
