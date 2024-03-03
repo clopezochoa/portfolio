02f5bc9c6dda8bb31aced4ae5d7a8ce29797c3fa
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-03 11:32:13 +0100

    set dark bg as def

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className="dark-bg">
+        <body className="bg-dark">
           {children}
         </body>
       </Providers>
