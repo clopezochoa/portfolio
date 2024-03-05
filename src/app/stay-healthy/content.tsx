49c58dd7b8ea4b76af1a0ba5942b93a92c4a3f9b
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-05 13:31:28 +0100

    stay healthy content

---
+++ src/app/stay-healthy/content.tsx
@@ -0,0 +1,32 @@
+export const initialBody = [
+  <>
+    <span>After prototyping the capstone project of a course by IBM about frontend engineering,<br/>
+    a React app had to be made to grant functionality to a static design in plain HTML and CSS.<br/>
+    Since my first visit to react.dev I realized that next.js was my best chance to deploy all<br/>
+    the typescript I have learnt over the last years while working as both front end and backend<br/>
+    developer.<br/>
+    <br/>
+    To elevate the experience of creating the app, I underwent a brief investigation on user personas around me.<br/>
+    What caught my attention was the increasing medical services for a broad variety of users, some of which<br/>
+    are not technologically savvy.<br/>
+    <br />
+    The final decision on what the app should do can be sythesized as:<br />
+    &quot;Medical sevices with ease&quot;.</span>
+  </>,
+  <>
+    <span className='bold'>Duration</span><br/>
+    <span>2 Months</span><br/>
+  </>,
+  <>
+    <span className='bold'>Tools</span><br/>
+    <span>Typescript + Next.js + MongoDB</span><br/>
+  </>,
+  <>
+    <span className='bold'>Role</span><br/>
+    <span>Frontend Developer</span><br/>
+  </>,
+  <>
+    <span className='bold'>Years</span><br/>
+    <span>2024</span><br/>
+  </>
+];
