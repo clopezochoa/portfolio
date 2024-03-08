36068e0f45986efb9336076edddaf3c2a359d057
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-08 18:19:59 +0100

    simplify routes enum

--- src/lib/routeList.ts
+++ src/lib/routeList.ts
@@ -1,36 +1,17 @@
-interface RouteGroup {
-  left: Route,
-  right: Route,
-  spare: Route
-}
-
-function createRouteGroup(left?: Route, right?: Route, spare?: Route):RouteGroup {
-  return {left: left ?? Route.home, right: right ?? Route.home, spare: spare ?? Route.home};
+export enum Pages {
+  generativeAlgorithm = "generative-algorithm",
+  engine = "engine",
+  frontend = "frontend",
+  stayHealthy = "stay-healthy",
+  about = "about",
 }
 
 export enum Route {
   home = "/",
-  aboutMe = "/about-me",
-  whatIDo = "/what-i-do",
-  frontend = "frontend",
+  generativeAlgorithm = `/#${Pages.generativeAlgorithm}`,
+  engine = `/${Pages.engine}`,
+  frontend = `/#${Pages.frontend}`,
+  stayHealthy = `/${Pages.stayHealthy}`,
   threeD = "/three-d",
-  generativeAlgorithm = "/generative-algorithm",
-  engine = "/engine"
-}
-
-export function getRouteGroup(route: Route): RouteGroup {
-  switch (route) {
-    case Route.home:
-      return createRouteGroup();
-    case Route.aboutMe:
-      return createRouteGroup();
-    case Route.whatIDo:
-      return createRouteGroup();
-      case Route.generativeAlgorithm:
-        return createRouteGroup(Route.engine);
-      case Route.engine:
-        return createRouteGroup(undefined, Route.generativeAlgorithm);
-      default:
-      return createRouteGroup();
-  }
+  about = `/#${Pages.about}`,
 }
