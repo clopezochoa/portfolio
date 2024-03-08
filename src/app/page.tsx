c6835d4016b93e9258a2e3ad8936736923a43161
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-08 18:25:03 +0100

    add about at bottom of homepage and use scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,42 +9,48 @@ import '@/styles/utils.css'
 import '@/styles/text.css'
 import GenerativeAlgorithm from "./generative-algorithm/content";
 import Window from "@/app/ui/window";
-import { useRouter } from "next/navigation";
 import { defaultColors } from "../../tailwind.config";
 import FrontendDevelopment from "./frontend/content";
+import { Greeting, WhatIDo } from "./about/content";
+import { Pages } from "@/lib/routeList";
+import { useScroll } from "./hooks/useScroll";
 
 export default function Home() {
   const fonts = useContext(FontsContext);
-  const router = useRouter();
-  const mainDiv = useRef(null);
-
-  const showAboutMe = () => {
-    (mainDiv.current! as HTMLDivElement).className = "grid slide-down";
-    
-    setTimeout(() => {
-      router.push("/about");
-    }, 400);
-  }
-
+  const windowLandingRef = useRef(null);
+  const generativeAlgorithmRef = useRef(null);
+  const frontendDevelopmentRef = useRef(null);
+  const aboutRef = useRef(null);
+  const refs = [windowLandingRef, generativeAlgorithmRef, frontendDevelopmentRef, aboutRef];
+  useScroll(refs);
+  
   return (<>
-    <div ref={mainDiv} className="grid">
-      <Window className="flex">
-        <div className="arrow-deck">
-          <Arrow strokeColor={defaultColors.clear} width={"auto"} height={60} stroke="1pt" orientation='up' className='arrow' onClick={() => showAboutMe()}/>
-          <div/>
-        </div>
-        <div className="landing-container">
-          <Name name="Carlos" surname="López-Ochoa" font={fonts.serif} />
-          <Landing font={fonts.serif} />
-        </div>
-      </Window>
-      <Window className="space-between">
-        <GenerativeAlgorithm />
-      </Window>
-      <Window className="space-between">
-        <FrontendDevelopment />
-      </Window>
-    </div>
+    <Window ref={windowLandingRef} className="flex">
+      <div className="landing-container">
+        <Name name="Carlos" surname="López-Ochoa" font={fonts.serif} />
+        <Landing font={fonts.serif} />
+      </div>
+      <div className="arrow-deck">
+        <div/>
+          <a href={`#${Pages.generativeAlgorithm}`}>
+            <Arrow strokeColor={defaultColors.clear} width={"auto"} height={60} stroke="1pt" orientation='down' className='arrow wave-opacity'/>
+          </a>
+      </div>
+    </Window>
+    <Window ref={generativeAlgorithmRef} className="space-between">
+      <GenerativeAlgorithm />
+    </Window>
+    <Window ref={frontendDevelopmentRef} className="space-between">
+      <FrontendDevelopment />
+    </Window>
+    <div style={{height:"100px"}}/>
+    <Window ref={aboutRef}>
+      <Greeting/>
+      <WhatIDo/>
+    </Window>
+    <a style={{display:"flex", justifyContent:"center"}} href={`#`}>
+      <Arrow strokeColor={defaultColors.dark} width={"auto"} height={60} stroke="1pt" orientation='up' className='arrow'/>
+    </a>
   </>
   );
 }
