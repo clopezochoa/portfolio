18110780b81ef9835058279eec01765e0f3e51cc
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-10 11:26:11 +0100

    vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 import { keywords } from "./content";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Carlos López-Ochoa",
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <Providers>
         <body className="bg-dark">
           {children}
