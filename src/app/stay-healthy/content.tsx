44d191dc5a492f532f17bc9ea4f106ef97467eba
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-11 16:16:50 +0100

    stay healthy content refactor

--- src/app/stay-healthy/content.tsx
+++ src/app/stay-healthy/content.tsx
@@ -1,6 +1,21 @@
+import '@/styles/text.css'
+
+enum Section {
+  design = "Design",
+  implement = "Implement",
+  connect = "Connect",
+  wireframing = "Wireframing",
+  staticDesign = "Static Design",
+  navigation = "Navigation",
+  services = "Services",
+  database = "Database",
+  api = "API",
+  cloud = "Cloud",
+}
+
 export const initialBody = [
   <>
-    <span>After prototyping the capstone project of a course by IBM about frontend engineering,<br/>
+    <span className='work-body'>After prototyping the capstone project of a course by IBM about frontend engineering,<br/>
     a React app had to be made to grant functionality to a static design in plain HTML and CSS.<br/>
     Since my first visit to react.dev I realized that next.js was my best chance to deploy all<br/>
     the typescript I have learnt over the last years while working as both front end and backend<br/>
@@ -14,38 +29,38 @@ export const initialBody = [
     &quot;Medical sevices with ease&quot;.</span>
   </>,
   <>
-    <span className='bold'>Duration</span><br/>
-    <span>2 Months</span><br/>
+    <span className='bold mt-20'>Duration</span><br/>
+    <span className='work-body'>2 Months</span><br/>
   </>,
   <>
     <span className='bold'>Tools</span><br/>
-    <span>Typescript + Next.js + MongoDB</span><br/>
+    <span className='work-body'>Typescript + Next.js + MongoDB</span><br/>
   </>,
   <>
     <span className='bold'>Role</span><br/>
-    <span>Frontend Developer</span><br/>
+    <span className='work-body'>Frontend Developer</span><br/>
   </>,
   <>
     <span className='bold'>Years</span><br/>
-    <span>2024</span><br/>
+    <span className='work-body'>2024</span><br/>
   </>
 ];
 
 
 export const introBody = [
   <>
-    <span className='bold large'>Introduction</span><br/>
+    <span className='work-subtitle'>Introduction</span><br/>
     <br />
-    <span>&quot;Create a simple medical appointment service&quot;.<br/>
+    <span className='work-body'>&quot;Create a simple medical appointment service&quot;.<br/>
     <br/>
     While wireframing the navigation bar, the basic pillars of this app emerged as main drivers of the whole experience.<br/>
     In opposition to cluttered UI designs observed after a desk research, simplicity is used to reduce bounce rates.</span>
   </>,
   <>
-    <span className='bold large'>Problem</span><br/>
+    <span className='work-subtitle'>Problem</span><br/>
     <br />
-    <span>Elder people may find web-apps cluttered, messy and not worthy of the effort.<br/>
-    How can fronttend development ease the pain of a cluttered UI?<br/>
+    <span className='work-body'>Elder people may find web-apps cluttered, messy and not worthy of the effort.<br/>
+    How can frontend development ease the pain of a cluttered UI?<br/>
     What good examples are there already?<br/>
     Can a simple design contribute to code maintainability?<br/>
     </span>
@@ -54,38 +69,46 @@ export const introBody = [
 
 export const processBody = [
   <>
-    <span className='bold large'>Process</span><br/>
+    <span className='work-subtitle'>Process</span><br/>
   </>,
   <>
     <span>
-    <span className="bold">DESIGN</span><br/>
-      <span className="ms-3">Wireframing<br/></span>
-      <span className="ms-3">Static Design<br/></span>
+    <span className="bold"><a href={'#' + Section.design}>{Section.design.toUpperCase()}</a></span><br/>
+      <div className='work-body'>
+      <a href={'#' + Section.wireframing} className="ms-3">{Section.wireframing}<br/></a>
+      <a href={'#' + Section.staticDesign} className="ms-3">{Section.staticDesign}<br/></a>
+      </div>
+      <br />
     </span>
   </>,
   <>
     <span>
-    <span className="bold">IMPLEMENT</span><br/>
-      <span className="ms-3">Navigation<br/></span>
-      <span className="ms-3">Services<br/></span>
+    <span className="bold"><a href={'#' + Section.implement}>{Section.implement.toUpperCase()}</a></span><br/>
+    <div className='work-body'>
+      <a href={'#' + Section.navigation} className="ms-3">{Section.navigation}<br/></a>
+      <a href={'#' + Section.services} className="ms-3">{Section.services}<br/></a>
+    </div>
+      <br />
     </span>
   </>,
   <>
     <span>
-    <span className="bold">CONNECT</span><br/>
-      <span style={{height:'2px'}}></span>
-      <span className="ms-3">Database<br/></span>
-      <span className="ms-3">API<br/></span>
-      <span className="ms-3">Cloud<br/></span>
+    <span className="bold"><a href={'#' + Section.connect}>{Section.connect.toUpperCase()}</a></span><br/>
+      <div className='work-body'>
+      <a href={'#' + Section.database} className="ms-3">{Section.database}<br/></a>
+      <a href={'#' + Section.api} className="ms-3">{Section.api}<br/></a>
+      <a href={'#' + Section.cloud} className="ms-3">{Section.cloud}<br/></a>
+      </div>
+      <br />
     </span>
   </>,
 ];
 
 export const designBody = [
   <>
-    <span className='bold large'>Wireframing</span><br/>
+    <span id={Section.wireframing} className='work-subtitle'>Wireframing</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       The approach that this design process followed had a top-to-bottom flow.<br/>
       A navigation bar is a great prototyping tool to encapsulate the pillars of an app:<br/>
       <br />
@@ -108,7 +131,7 @@ export const designBody = [
       Since these services offer tailored information for the user, signing in and up must be at hand.<br/>
       This implies that a user model and a session context must be created and managed throught cookies.<br/>
     </span><br/>
-    <span>
+    <span className='work-body'>
       Up to this point, a wishlist can be pulled out of the navbar:<br/>
       We need a database to retain the information.<br/>
       API Endpoints to get and post info to it.<br/>    
@@ -116,13 +139,13 @@ export const designBody = [
     </span><br/>
   </>,
   <>
-    <span className='bold large'>Static Design</span><br/>
+    <span id={Section.staticDesign} className='work-subtitle'>Static Design</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       What comes after initial drafts is to produce a responsive static desing for both desktop and mobile. Even though users of advanced age are getting more and more<br />
       into the mobile scene using smartphones, they often rely on computers simply because they were here before smartphones. <br />
     </span> <br />
-    <span>
+    <span className='work-body'>
       Figma was a great aid when it came to:<br/>  
       <br />
       - Streamline the components production. Since React is component based, Figma&quot;s components are a blessing to keep everything tidy.<br/>
@@ -135,18 +158,18 @@ export const designBody = [
 
 export const implementBody = [
   <>
-    <span className='bold large'>Navigation</span><br/>
+    <span id={Section.navigation} className='work-subtitle'>Navigation</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       First and foremost, it has to perform way faster and process way more data than the previous pipeline.<br/>  
       Robustness is also key to allow the algorithm to be reused over and over with any client, may it be very small or gigantic.<br/>  
       Robotic and artificial patterns must be avoided, the look and feel must be organic and should offer an image to be remembered.<br/>    
     </span>
   </>,
   <>
-    <span className='bold large'>Services</span><br/>
+    <span id={Section.services} className='work-subtitle'>Services</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       After testing a new possible pipeline using simple JSON files and not .obj or .glb files, it faced two critical tests.<br/>    
       Simulation time is the obvious one. Processing taking longer than ten minutes triggers alerts and may require optimization.<br/>    
       The second challenge is Babylon build time. Previously, bundled 3D objects resulted in fetching a file exceeding 100 MB.<br/>
@@ -164,18 +187,18 @@ export const implementBody = [
 
 export const connectBody = [
   <>
-    <span className='bold large'>Database</span><br/>
+    <span id={Section.database} className='work-subtitle'>Database</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       The unit chosen to build models is a very simple hexagon built by seven points in space, in the XZ plane, the ground.<br/>  
       That means we just have to set X and Z coordinates of the center of each unit relative to an origin, which is zero, zero, zero.<br/>  
       Knowing that, along with the radius of the hexagon, I could lay down elements to an elemental grid.<br/>    
     </span>
   </>,
   <>
-    <span className='bold large'>API</span><br/>
+    <span id={Section.api} className='work-subtitle'>API</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       How to get a first bundle and make it resemble organic from early on? Soon I realized that I needed a shape algorithm to rig the whole simulation.<br/>
       Answering that question was fun because initially I used no initial shape rig and let the simulation decide where to grow freely.<br/>    
       It led me to very questionable results. Basically a greater hexagon, a round circle or a rhomboid. No matter how I told the units to grow,<br/>    
@@ -193,9 +216,9 @@ export const connectBody = [
     </span>
   </>,
   <>
-    <span className='bold large'>Cloud</span><br/>
+    <span id={Section.cloud} className='work-subtitle'>Cloud</span><br/>
     <br />
-    <span>
+    <span className='work-body'>
       One last thing before starting to code, after prototyping, was to give a seed to each tenant. That way, everything was deterministic and even if<br />
       some decisions may be random, it all can be traced, reproduced and debugged. It also helped differentiating one customer from another, by being sure<br />
       about at least one parameter that each generation is unique to its tenant and cannot be mistaken.<br />
