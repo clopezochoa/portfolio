fb8a500df722203d313724c17b4c74db66f29621
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-12 13:12:31 +0100

    wireframe upgrades

--- src/app/ui/png/Wireframe.tsx
+++ src/app/ui/png/Wireframe.tsx
@@ -1,20 +1,28 @@
 'use client'
 
 import Image from "next/image";
+import { CSSProperties } from "react";
 
-const Wireframe = ({src, className, margin}:{src:string, className: string, margin:string}) => {
-  const src_list = Array.from(Array(4)).map((item, index) => {
+const Wireframe = ({itemStyle, containerStyle, src, margin, count, format, pick}:{pick?:number, itemStyle:CSSProperties, containerStyle:CSSProperties, src:string, margin:string, count:number, format:string}) => {
+  const src_list = Array.from(Array(count)).map((item, index) => {
     return(src +"-0" + (index +1).toString());
-  })
-  return (
-  <div className={className}>
+  });
+
+  const content = pick ?
+    <div style={containerStyle}>
+        <div style={{margin:margin}}>
+          <Image style={itemStyle} src= {`/${format}/` + src_list[pick -1] + `.${format}`} alt={src} width={1000} height={1000}/>
+        </div>
+    </div>
+    : 
+    <div style={containerStyle}>
       {src_list.map((srcItem, key) => {
         return (<div style={{margin:margin}} key={key}>
-          <Image src= {"/png/" + srcItem + ".png"} alt={src} width={1000} height={1000}/>
+          <Image style={itemStyle} src= {`/${format}/` + srcItem + `.${format}`} alt={src} width={1000} height={1000}/>
         </div>)
       })}
-    </div>
-  );
+    </div>;
+  return (content);
 };
 
 export default Wireframe;
